893201cdd9cbbad56c1941a61964e20f0d8e4f1a
Krishna Pandey <kpandey81930@gmail.com>
2025-07-20 20:51:10 +0530

    chore: clang-format
    Signed-off-by: Krishna Pandey <kpandey81930@gmail.com>

--- libc/src/math/generic/bf16fmaf.cpp
+++ libc/src/math/generic/bf16fmaf.cpp
@@ -8,9 +8,9 @@
 
 #include "src/math/bf16fmaf.h"
 
-#include "src/__support/common.h"
-#include "src/__support/FPUtil/bfloat16.h"
 #include "src/__support/FPUtil/FMA.h"
+#include "src/__support/FPUtil/bfloat16.h"
+#include "src/__support/common.h"
 #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
